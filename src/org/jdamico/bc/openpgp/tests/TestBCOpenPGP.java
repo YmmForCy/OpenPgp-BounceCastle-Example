03528e9f2c00a29afb2f280c8b5551e37d326f55
chenyu <cyu0712@163.com>
2019-04-15 15:43:49 +0800

    修改路径

--- src/org/jdamico/bc/openpgp/tests/TestBCOpenPGP.java
+++ src/org/jdamico/bc/openpgp/tests/TestBCOpenPGP.java
@@ -1,4 +1,5 @@
 package org.jdamico.bc.openpgp.tests;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,72 +20,75 @@ import org.junit.Test;
 
 public class TestBCOpenPGP {
 
-	private boolean isArmored = false;
-	private String id = "damico";
-	private String passwd = "******";
-	private boolean integrityCheck = true;
+    private boolean isArmored = false;
+    private String id = "damico";
+    private String passwd = "******";
+    private boolean integrityCheck = true; // 完整性检查
 
 
-	private String pubKeyFile = "/tmp/pub.dat";
-	private String privKeyFile = "/tmp/secret.dat";
+    private String pubKeyFile = "/Users/chenyu/Desktop/PGP/pub.dat";
+    private String privKeyFile = "/Users/chenyu/Desktop/PGP/secret.dat";
 
-	private String plainTextFile = "/tmp/plain-text.txt"; //create a text file to be encripted, before run the tests
-	private String cipherTextFile = "/tmp/cypher-text.dat";
-	private String decPlainTextFile = "/tmp/dec-plain-text.txt";
-	private String signatureFile = "/tmp/signature.txt";
+    private String plainTextFile = "/Users/chenyu/Desktop/PGP/plain-text.txt"; // create a text file to be encripted, before run the tests
+    private String cipherTextFile = "/Users/chenyu/Desktop/PGP/cypher-text.txt"; // 加密文件
+    private String decPlainTextFile = "/Users/chenyu/Desktop/PGP/dec-plain-text.txt"; // 解密后的文件
+    private String signatureFile = "/Users/chenyu/Desktop/PGP/signature.txt"; //签名文件
 
-	@Test
-	public void genKeyPair() throws InvalidKeyException, NoSuchProviderException, SignatureException, IOException, PGPException, NoSuchAlgorithmException {
+    @Test
+    public void genKeyPair() throws InvalidKeyException, NoSuchProviderException, SignatureException, IOException, PGPException, NoSuchAlgorithmException {
 
-		RSAKeyPairGenerator rkpg = new RSAKeyPairGenerator();
+        RSAKeyPairGenerator rkpg = new RSAKeyPairGenerator();
 
-		Security.addProvider(new BouncyCastleProvider());
+        // 添加BouncyCastleProvider，是一种用于 Java 平台的开放源码的轻量级密码术包，它支持大量的密码术算法
+        Security.addProvider(new BouncyCastleProvider());
 
-		KeyPairGenerator    kpg = KeyPairGenerator.getInstance("RSA", "BC");
+        // 获取KeyPairGenerator，算法名称：RSA、Provider：BC = BouncyCastleProvider
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", "BC");
 
-		kpg.initialize(1024);
+        // 初始化秘钥长度
+        kpg.initialize(1024);
 
-		KeyPair                    kp = kpg.generateKeyPair();
+        // 生成秘钥对
+        KeyPair kp = kpg.generateKeyPair();
 
-		FileOutputStream    out1 = new FileOutputStream(privKeyFile);
-		FileOutputStream    out2 = new FileOutputStream(pubKeyFile);
+        FileOutputStream out1 = new FileOutputStream(privKeyFile);
+        FileOutputStream out2 = new FileOutputStream(pubKeyFile);
 
-		rkpg.exportKeyPair(out1, out2, kp.getPublic(), kp.getPrivate(), id, passwd.toCharArray(), isArmored);
+        // 导出秘钥对 （公钥目录、私钥目录、公钥、私钥、id、passwd、isArmored）
+        rkpg.exportKeyPair(out1, out2, kp.getPublic(), kp.getPrivate(), id, passwd.toCharArray(), isArmored);
+    }
 
+    @Test
+    public void encrypt() throws NoSuchProviderException, IOException, PGPException {
+        FileInputStream pubKeyIs = new FileInputStream(pubKeyFile);
+        FileOutputStream cipheredFileIs = new FileOutputStream(cipherTextFile);
+        PgpHelper.getInstance().encryptFile(cipheredFileIs, plainTextFile, PgpHelper.getInstance().readPublicKey(pubKeyIs), isArmored, integrityCheck);
+        cipheredFileIs.close();
+        pubKeyIs.close();
+    }
 
-	}
+    @Test
+    public void decrypt() throws Exception {
 
-	@Test
-	public void encrypt() throws NoSuchProviderException, IOException, PGPException{
-		FileInputStream pubKeyIs = new FileInputStream(pubKeyFile);
-		FileOutputStream cipheredFileIs = new FileOutputStream(cipherTextFile);
-		PgpHelper.getInstance().encryptFile(cipheredFileIs, plainTextFile, PgpHelper.getInstance().readPublicKey(pubKeyIs), isArmored, integrityCheck);
-		cipheredFileIs.close();
-		pubKeyIs.close();
-	}
+        FileInputStream cipheredFileIs = new FileInputStream(cipherTextFile);
+        FileInputStream privKeyIn = new FileInputStream(privKeyFile);
+        FileOutputStream plainTextFileIs = new FileOutputStream(decPlainTextFile);
+        PgpHelper.getInstance().decryptFile(cipheredFileIs, plainTextFileIs, privKeyIn, passwd.toCharArray());
+        cipheredFileIs.close();
+        plainTextFileIs.close();
+        privKeyIn.close();
+    }
 
-	@Test
-	public void decrypt() throws Exception{
+    @Test
+    public void signAndVerify() throws Exception {
+        FileInputStream privKeyIn = new FileInputStream(privKeyFile);
+        FileInputStream pubKeyIs = new FileInputStream(pubKeyFile);
+        FileInputStream plainTextInput = new FileInputStream(plainTextFile);
+        FileOutputStream signatureOut = new FileOutputStream(signatureFile);
 
-		FileInputStream cipheredFileIs = new FileInputStream(cipherTextFile);
-		FileInputStream privKeyIn = new FileInputStream(privKeyFile);
-		FileOutputStream plainTextFileIs = new FileOutputStream(decPlainTextFile);
-		PgpHelper.getInstance().decryptFile(cipheredFileIs, plainTextFileIs, privKeyIn, passwd.toCharArray());
-		cipheredFileIs.close();
-		plainTextFileIs.close();
-		privKeyIn.close();
-	}
-
-	@Test
-	public void signAndVerify() throws Exception{
-		FileInputStream privKeyIn = new FileInputStream(privKeyFile);
-		FileInputStream pubKeyIs = new FileInputStream(pubKeyFile);
-		FileInputStream plainTextInput = new FileInputStream(plainTextFile);
-		FileOutputStream signatureOut = new FileOutputStream(signatureFile);
-				
-		byte[] bIn = PgpHelper.getInstance().inputStreamToByteArray(plainTextInput);
-		byte[] sig = PgpHelper.getInstance().createSignature(plainTextFile, privKeyIn, signatureOut, passwd.toCharArray(), true);
-		PgpHelper.getInstance().verifySignature(plainTextFile, sig, pubKeyIs);
-	}
+        byte[] bIn = PgpHelper.getInstance().inputStreamToByteArray(plainTextInput);
+        byte[] sig = PgpHelper.getInstance().createSignature(plainTextFile, privKeyIn, signatureOut, passwd.toCharArray(), true);
+        PgpHelper.getInstance().verifySignature(plainTextFile, sig, pubKeyIs);
+    }
 
 }
